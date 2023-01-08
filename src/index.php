238047b412eda327b1c60d898f21ed7629f1caf4
torres-developer <torres.dev@disroot.org>
2023-01-08 00:25:19 +0000

    remove index

--- src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TorresDeveloper\HTTPMessage\Request;
-use TorresDeveloper\Pull\Pull;
-
-require_once __DIR__ .  "/../vendor/autoload.php";
-var_dump(Pull::fetch()->start(new Request(
-    "https://example.org/"
-))->getBody()->getContents());
